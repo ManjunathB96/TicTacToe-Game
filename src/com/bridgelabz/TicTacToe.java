cc284ee50d4564652f761d100300842b84db4624
Manju31051996 <bbelagavi6@gmail.com>
2022-11-17 22:21:47 +0530

    [Manjunath S B] added the UC1 file to TicTacToe Repo

---
+++ src/com/bridgelabz/TicTacToe.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
+
+        printBoard(gameBoard);
+    }
+
+    public static void printBoard(char[][] gameBoard) {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+}
+
